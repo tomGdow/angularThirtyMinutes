d645b103ca923de18255ab401e0b77b940daf867
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-04-13 11:44:39 +0100

    added angular data binding example

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
 
+function dragDiv() {
+    $('#scorewrapper').draggable();
+}
+
+$( document ).ready(function() {
+    dragDiv();
+});
+
+
+var mymodule = (function () {
+//Private
+
+//Public
+    return {
+        $$: function (id) {
+            'use strict';
+            if (typeof id != 'undefined') {
+                return document.getElementById(id);
+            }
+            else {
+                return 'undefined'
+            }
+        },
+
+        showDivs: function (arg) {
+            document.getElementById(arg).style.visibility = 'visible';
+        },
+
+        hideDivs: function (arg) {
+            document.getElementById(arg).style.visibility = 'hidden';
+        }
+
+    }
+})();
+
+
+//AddEventListener
+mymodule.$$('myinput').addEventListener('focus',function () {
+
+    mymodule.showDivs('scorewrapper');
+    return false;
+}, false);
+
+mymodule.$$('myinput').addEventListener('blur',function () {
+
+    mymodule.hideDivs('scorewrapper');
+    return false;
+}, false);
